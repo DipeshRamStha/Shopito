50c2bb683360545059a9369c2b7151bfb06d9c0f
DipeshRamStha <visit.dipesh933@gmail.com>
2024-08-21 15:26:51 -0500

    Added the dotenv express mongoose cors cookieParser in backend server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,34 @@
-console.log("Hello World!!!");
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+// MiddleWares
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000/", "https://shopitoapp.vercel.app"],
+    credentials: true,
+  })
+);
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Home Page...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
