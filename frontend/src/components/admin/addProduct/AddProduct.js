07427a04e50e38dae1a957ccc49729149eb14a18
DipeshRamStha <visit.dipesh933@gmail.com>
2024-11-24 23:43:50 -0600

    November 24 2024 After the Git Bus Error -> AddProduct js file created

--- frontend/src/components/admin/addProduct/AddProduct.js
+++ frontend/src/components/admin/addProduct/AddProduct.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import Loader from "../../loader/Loader";
 
 const AddProduct = () => {
-  return <div>AddProduct</div>;
+  const { isLoading } = useSelector((state) => state.product);
+
+  return (
+    <section>
+      <div className="container">
+        {isLoading && <Loader />}
+        <h3 className="--mt">Add New Product</h3>
+      </div>
+    </section>
+  );
 };
 
 export default AddProduct;
