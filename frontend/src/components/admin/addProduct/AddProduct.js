63561ab30cd162c9f97e730c31cf22593dd8545d
DipeshRamStha <visit.dipesh933@gmail.com>
2024-12-02 03:49:33 -0600

    December 02 2024 created AddProduct with filteredBrands

--- frontend/src/components/admin/addProduct/AddProduct.js
+++ frontend/src/components/admin/addProduct/AddProduct.js
@@ -1,15 +1,69 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import "./AddProduct.scss";
+import { useDispatch, useSelector } from "react-redux";
 import Loader from "../../loader/Loader";
+import ProductForm from "../productForm/ProductForm";
+import { getCategories } from "../../../redux/features/categoryAndBrand/categoryAndBrandSlice";
+
+const initialState = {
+  name: "",
+  category: "",
+  brand: "",
+  quantity: "",
+  color: "",
+  price: "",
+  regularPrice: "",
+};
 
 const AddProduct = () => {
+  const dispatch = useDispatch();
+  const [product, setProduct] = useState(initialState);
+  const [filteredBrands, setFilteredBrands] = useState([]);
+
   const { isLoading } = useSelector((state) => state.product);
+  const { categories, brands } = useSelector((state) => state.category);
+
+  const { name, category, brand, price, quantity, color, regularPrice } =
+    product;
+
+  useEffect(() => {
+    dispatch(getCategories());
+  }, [dispatch]);
+
+  // Filter Brands based on selectedCategory
+  const filterBrands = (selectedCategory) => {
+    const newBrands = brands.filter(
+      (brand) => brand.category === selectedCategory
+    );
+    setFilteredBrands(newBrands);
+  };
+  useEffect(() => {
+    filterBrands(category);
+  }, [category]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setProduct({ ...product, [name]: value });
+  };
+
+  const saveProduct = async (e) => {
+    e.preventDefault();
+    console.log(product);
+  };
 
   return (
     <section>
       <div className="container">
         {isLoading && <Loader />}
         <h3 className="--mt">Add New Product</h3>
+        <ProductForm
+          saveProduct={saveProduct}
+          product={product}
+          handleInputChange={handleInputChange}
+          categories={categories}
+          isEditing={false}
+          filteredBrands={filteredBrands}
+        />
       </div>
     </section>
   );
--- frontend/src/components/admin/productForm/ProductForm.js
+++ frontend/src/components/admin/productForm/ProductForm.js
@@ -1,7 +1,76 @@
 import React from "react";
+import "./ProductForm.scss";
+import Card from "../../card/Card";
 
-const ProductForm = () => {
-  return <div>ProductForm</div>;
+const ProductForm = ({
+  saveProduct,
+  product,
+  handleInputChange,
+  categories,
+  isEditing,
+  filteredBrands,
+}) => {
+  return (
+    <div className="add-product">
+      <h3>Upload Widget Placeholder</h3>
+      <Card cardClass={"card"}>
+        <br />
+        <form onSubmit={saveProduct}>
+          <label>Product Name:</label>
+          <input
+            type="text"
+            placeholder="Product name"
+            name="name"
+            value={product?.name}
+            onChange={handleInputChange}
+          />
+          <label>Product Category:</label>
+          <select
+            name="category"
+            value={product?.category}
+            onChange={handleInputChange}
+          >
+            {isEditing ? (
+              <option value={product?.category}>{product?.category}</option>
+            ) : (
+              <option>select category</option>
+            )}
+            {categories.length > 0 &&
+              categories.map((cat) => (
+                <option key={cat._id} value={cat.name}>
+                  {cat.name}
+                </option>
+              ))}
+          </select>
+
+          <label>Product Brand:</label>
+          <select
+            name="brand"
+            value={product?.brand}
+            onChange={handleInputChange}
+          >
+            {isEditing ? (
+              <option value={product?.brand}>{product?.brand}</option>
+            ) : (
+              <option>select brand</option>
+            )}
+            {filteredBrands.length > 0 &&
+              filteredBrands.map((brand) => (
+                <option key={brand._id} value={brand.name}>
+                  {brand.name}
+                </option>
+              ))}
+          </select>
+
+          <div className="--my">
+            <button type="submit" className="--btn --btn-primary">
+              Save Product
+            </button>
+          </div>
+        </form>
+      </Card>
+    </div>
+  );
 };
 
 export default ProductForm;
